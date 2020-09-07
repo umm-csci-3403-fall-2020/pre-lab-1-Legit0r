f1eb4aa20d9f38135fccb6aee6eeabb9541257a8
Legit0r <michaelfairbanks5469@gmail.com>
2020-09-07 18:08:34 -0500

    Add regex.sh

---
+++ regex/regex.sh
@@ -0,0 +1,5 @@
+sed -E 's/\* ([[:alpha:]]+), ([[:alpha:]]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+
+sed -E  -n 's/\* I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+)./1. \1\n2. \2\n/p' < r1_input.txt > r1_output.txt
+
+sed -E 's/\* sandwich with ([a-zA-Z.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
